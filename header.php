ce365e0427234b79c63452a09cf8e776048637c0
Imtiaz <imtiazali4090@gmail.com>
2020-05-29 15:39:57 +0600

    Removed files

--- header.php
+++
@@ -1,6 +0,0 @@
-<meta charset="utf-8" />
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<script src="js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
-<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
--- setMedSession.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['med_id']=$_GET['id'];
-echo $_SESSION['med_id'];
-?>  
